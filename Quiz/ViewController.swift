57d80582cdc2bb84a9ed19e96ed2cb421aa302af
steveharski <steveharski@gmail.com>
2017-10-30 16:41:25 +0200

    Animation: page 150

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,74 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Adminaccount on 10/11/17.
+//  Copyright © 2017 Adminaccount. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var currentQuestionLabel: UILabel!
+    @IBOutlet var currentQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var nextQuestionLabel: UILabel!
+    @IBOutlet var nextQuestionLabelCenterXConstraint: NSLayoutConstraint!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+    "What is 7+7?",
+    "What is the capital of Vermont?",
+    "What is cognac made from?"]
+    
+    let answers: [String] = [
+    "14",
+    "Montpelier",
+    "Grapes"]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        nextQuestionLabel.text = question
+        answerLabel.text = "???"
+        
+        animatedLabelTransitions()
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        currentQuestionLabel.text = questions[currentQuestionIndex]
+    }
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Set the label's initial alpha
+        nextQuestionLabel.alpha = 0
+    }
+    
+    func animatedLabelTransitions() {
+        // Animate the alpha
+        UIView.animate(withDuration: 0.5, delay: 0, options: [], animations: {
+            self.currentQuestionLabel.alpha = 0
+            self.nextQuestionLabel.alpha = 1
+        }, completion: { _ in
+            swap(&self.currentQuestionLabel, &self.nextQuestionLabel)
+        })
+    }
+    
+
+}
+
